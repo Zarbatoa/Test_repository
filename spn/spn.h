8d41ce11e9f9553e9a4694a9b174abde256ab8dd
zarbatoa <zarbatoa@users.noreply.github.com>
2015-06-09 15:22:30 -0300

    sub_keys function added in spn.h and aub_keys_test.cpp created

--- spn/spn.h
+++ spn/spn.h
@@ -1,5 +1,9 @@
 #ifndef __SPN_H_
 #define __SPN_H_
+#include <vector>
+
+typedef std::uint_least32_t word;
+typedef std::uint_least16_t sub_key;
 
 int permutation_s[16] = {
     14, 4, 13, 1, 2, 15, 11, 8,
@@ -10,5 +14,17 @@ int permutation_p[16] = {
     1, 5, 9, 13, 2, 6, 10, 14,
     3, 7, 11, 15, 4, 8, 12, 16
 };
+
+void sub_keys(std::vector<sub_key> & sub_keys, word key)
+{
+    for (int r = 1; r < 6; r++) {
+        sub_key kr = (key << (4*r-4)) >> 16;
+        sub_keys.insert( sub_keys.end(), kr);
+    }
+}
+
+word spn_cipher (word plain, word key)
+{
+}
 
 #endif
---
+++ spn/sub_keys_test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include "spn.h"
+
+using namespace std;
+
+int main(void) {
+    word key = 0b0000'0110'1111'1010'0000'1100'0011'0000;
+    //sub_key_1 => 0000 0110 1111 1010  => 1786
+    //sub_key_2 => 0110 1111 1010 0000  => 28576
+    //sub_key_3 => 1111 1010 0000 1100  => 64012
+    //sub_key_4 => 1010 0000 1100 0011  => 41155
+    //sub_key_5 => 0000 1100 0011 0000  => 3120
+
+    vector<sub_key> keys;
+    sub_keys(keys,key);
+    cout << "Subkeys test: " << endl;
+    int j = 1;
+    for(auto i : keys)
+    {
+        cout << "Sub_key[" << j << "] = " << i << endl;
+        j++;
+    }
+
+    return 0;
+}
