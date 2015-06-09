326c3306b69000adc6f427059769587cc6ac1369
zarbatoa <zarbatoa@users.noreply.github.com>
2015-06-09 14:25:25 -0300

    Adding permutation_p and permutation_s into spn.h

--- spn/spn.h
+++ spn/spn.h
@@ -1,6 +1,14 @@
 #ifndef __SPN_H_
 #define __SPN_H_
 
+int permutation_s[16] = {
+    14, 4, 13, 1, 2, 15, 11, 8,
+    3, 10, 6, 12, 5, 9, 0, 7
+};
 
+int permutation_p[16] = {
+    1, 5, 9, 13, 2, 6, 10, 14,
+    3, 7, 11, 15, 4, 8, 12, 16
+};
 
 #endif
